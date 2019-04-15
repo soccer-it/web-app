4e9c199d791c7b22e75eb196a0ff8a4c40d50172
Milson Ramos de Carvalho Júnior <junior.milson17@gmail.com>
2019-04-15 03:11:01 -0300

    feat: add choose-your-team module base structure

--- project.clj
+++ project.clj
@@ -3,19 +3,20 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  
-  
+
+
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [org.clojure/core.async  "0.4.474"]
+                 [cljsjs/react-swipeable-views "0.12.18-0"]
                  [rum "0.11.2"]]
 
   :plugins [[lein-figwheel "0.5.16"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
             [lein-sass "0.4.0"]]
-  
+
   :sass {:src "src/cljs/soccer_it_webapp/styles/"
          :output-directory "resources/public/css/"}
 
@@ -56,20 +57,20 @@
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
-             
+
              :css-dirs ["resources/public/css"] ;; watch and update CSS
-             
+
              ;; Start an nREPL server into the running figwheel process
              ;; :nrepl-port 7888
-             
+
              ;; Server Ring Handler (optional)
              ;; if you want to embed a ring handler into the figwheel http-kit
              ;; server, this is for simple ring servers, if this
-             
+
              ;; doesn't work for you just run your own server :) (see lein-ring)
-             
+
              ;; :ring-handler hello_world.server/handler
-             
+
              ;; To be able to open files in your editor from the heads up display
              ;; you will need to put a script on your path.
              ;; that script will have to take a file path and a line number
@@ -78,16 +79,16 @@
              ;; emacsclient -n +$2 $1
              ;;
              ;; :open-file-command "myfile-opener"
-             
+
              ;; if you are using emacsclient you can just use
              ;; :open-file-command "emacsclient"
-             
+
              ;; if you want to disable the REPL
              ;; :repl false
-             
+
              ;; to configure a different figwheel logfile path
              ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             
+
              ;; to pipe all the output to the repl
              ;; :server-logfile false
              }
--- src/cljs/soccer_it_webapp/components/teams.cljs
+++ src/cljs/soccer_it_webapp/components/teams.cljs
@@ -1,5 +1,27 @@
 (ns components.teams
-  (:require [rum.core :as rum]))
+  (:require
+   [sablono.core :as sablono]
+   [rum.core :as rum]
+   [cljsjs.react-swipeable-views]))
+
+(rum/defc singleTeam []
+  [:figure.single-team
+   [:img.banner {:src "images/single-team-banner.svg"}]
+   [:figcaption.caption "Team Name"]])
+
+(rum/defc Swipeable [props children]
+  (js/React.createElement js/SwipeableViews #js {} (sablono/html children)))
 
 (rum/defc teams []
-  [:div.teams "Teams Component!"])
+  [:div.teams
+   [:div.wrapper
+    [:h1.title "Escolha seu Time"]
+    [:div {:class "items"}
+     (Swipeable {}
+                [(singleTeam {})
+                 (singleTeam {})
+                 (singleTeam {})
+                 (singleTeam {})
+                 (singleTeam {})
+                 (singleTeam {})
+                 (singleTeam {})])]]])
