202d9f997a6d06bcb3d9b13060bb393813ca1442
Milson Ramos de Carvalho Júnior <junior.milson17@gmail.com>
2019-04-21 21:55:34 -0300

    feat: add search base structure using mocked data on ChooseYourTeam component

---
+++ src/cljs/soccer_it_webapp/components/logic.cljs
@@ -0,0 +1,3 @@
+(ns components.teams.logic
+  (:require
+   [rum.core :as rum]))
--- src/cljs/soccer_it_webapp/components/teams.cljs
+++ src/cljs/soccer_it_webapp/components/teams.cljs
@@ -4,24 +4,38 @@
    [rum.core :as rum]
    [cljsjs.react-swipeable-views]))
 
-(rum/defc singleTeam []
+;;TODO => Remove Logic from view
+
+(rum/defc singleTeam [name]
   [:figure.single-team
    [:img.banner {:src "images/single-team-banner.svg"}]
-   [:figcaption.caption "Team Name"]])
+   [:figcaption.caption name]])
 
 (rum/defc Swipeable [props children]
   (js/React.createElement js/SwipeableViews #js {} (sablono/html children)))
 
-(rum/defc teams []
-  [:div.teams
-   [:div.wrapper
-    [:h1.title "Escolha seu Time"]
-    [:div {:class "items"}
-     (Swipeable {}
-                [(singleTeam {})
-                 (singleTeam {})
-                 (singleTeam {})
-                 (singleTeam {})
-                 (singleTeam {})
-                 (singleTeam {})
-                 (singleTeam {})])]]])
+(def teamsList [{:name "Corinthians" :slug "COR"}
+                {:name "Palmeiras" :slug "PAL"}
+                {:name "Santos" :slug "SAN"}
+                {:name "São Paulo" :slug "SAO"}])
+
+(defn getFilteredItems [value list]
+  (filter
+   (fn [team]
+     (re-find (re-pattern (str "(?i)" value)) (get team :name))) list))
+
+(rum/defcs teams
+  < rum/reactive
+  (rum/local teamsList ::filteredList)
+  [state]
+  (let [currentTeamList (::filteredList state)]
+    [:div.teams
+     [:div.search
+      [:input {:type "search"
+               :placeholder "Busque pelo seu time"
+               :onChange (fn [e] (swap! currentTeamList (fn [_] (getFilteredItems (.. e -target -value) teamsList))))}]]
+     [:div.wrapper
+      [:h1.title "Escolha seu Time"]
+      [:div {:class "items"}
+       (Swipeable {}
+                  [(for [team @currentTeamList] (singleTeam (get team :name)))])]]]))
