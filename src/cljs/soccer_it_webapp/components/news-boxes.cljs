18ab50576ecbc8f269f1248e567da6e8c4754f43
victufell <victor.fellype.a@gmail.com>
2019-04-28 15:31:47 -0300

    feat: added Swipeable in news

--- src/cljs/soccer_it_webapp/components/news-boxes.cljs
+++ src/cljs/soccer_it_webapp/components/news-boxes.cljs
@@ -4,6 +4,9 @@
             [cljsjs.react-swipeable-views]
             [components.news-box :refer [news-box]]))
 
+(rum/defc Swipeable [props children]
+  (js/React.createElement js/SwipeableViews #js {} (sablono/html children)))
+
 (def noticeList [{:title "Corinthians" :description "Corinthians toptopshowba"}
                 {:title "Palmeiras" :description "Palmeiras showbaless"}
                 {:title "Santos" :description "Santos toptop ou topshowba"}
@@ -11,5 +14,6 @@
 
 (rum/defc news-boxes []
   [:div {:class "news-boxes"}
+    (Swipeable {}
       (for [notice noticeList]
-        (news-box notice))])
+        (news-box notice)))])
