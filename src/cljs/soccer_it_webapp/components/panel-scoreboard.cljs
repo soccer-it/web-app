82cc928b2bc73eb67f600cbfc4d22b8cd1f47bbc
victufell <victor.fellype.a@gmail.com>
2019-05-04 15:57:45 -0300

    feat: added swipeable for matchs

--- src/cljs/soccer_it_webapp/components/panel-scoreboard.cljs
+++ src/cljs/soccer_it_webapp/components/panel-scoreboard.cljs
@@ -22,26 +22,29 @@
         :visitorSlug ""
       }
     },
+    :stadium "Castelão (CE)"},
+    {
+    :date "2019-05-01T21:30",
+    :time "21:30",
+    :principalScoreboard 0,
+    :visitorScoreboard 2,
+    :teams {
+      :principal {
+        :id "mMl10",
+        :name "Palmeiras",
+        :src "https://seeklogo.com/images/S/Sociedade_Esportiva_Palmeiras_de_Sao_Paulo-SP-logo-BE3E91E989-seeklogo.com.png",
+        :principalSlug ""
+      },
+      :visitor {
+        :id "Z1roetm",
+        :name "Corinthians",
+        :src "https://seeklogo.com/images/S/Sociedade_Esportiva_Palmeiras_de_Sao_Paulo-SP-logo-BE3E91E989-seeklogo.com.png" 
+        :visitorSlug ""
+      }
+    },
     :stadium "Castelão (CE)"}])
 
 (rum/defc panel-scoreboard []
-  (swipeable {}
-    (for [scores scoreboardList] ^{:key scores} (scoreboard scores))))
-
-
-  ;   [
-  ;   {:src "https://seeklogo.com/images/S/Sociedade_Esportiva_Palmeiras_de_Sao_Paulo-SP-logo-BE3E91E989-seeklogo.com.png" 
-  ;   :team-name "Palmeiras"
-  ;   :score 0}
-  ;   {:src "https://seeklogo.com/images/E/Esporte_Clube_Corinthians_de_Laguna-SC-logo-E82F2ADE11-seeklogo.com.png" 
-  ;   :team-name "Corinthians"
-  ;   :score 1}
-  ; ]
-  ; [
-  ;   {:src "https://seeklogo.com/images/S/Sociedade_Esportiva_Palmeiras_de_Sao_Paulo-SP-logo-BE3E91E989-seeklogo.com.png" 
-  ;   :team-name "Palmeiras"
-  ;   :score 0}
-  ;   {:src "https://seeklogo.com/images/E/Esporte_Clube_Corinthians_de_Laguna-SC-logo-E82F2ADE11-seeklogo.com.png" 
-  ;   :team-name "Corinthians"
-  ;   :score 1}
-  ; ]
+  [:div {:class "container -is-scoreboard"}
+    (swipeable {}
+      (for [scores scoreboardList] ^{:key scores} (scoreboard scores)))])
