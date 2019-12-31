6d77d47e2d27bf3e6a0f26887e2c9d7daf2902c3
Milson Ramos de Carvalho Júnior <milson.ramos@idwall.co>
2019-12-31 12:41:02 -0300

    refactor: ga helper

--- utils/ga.js
+++ utils/ga.js
@@ -5,8 +5,10 @@ export default function ga() {
     return;
   }
 
+  const id = 'UA-149910123-1';
+
   const script = document.createElement('script');
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-149910123-1';
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;
   document.body.appendChild(script);
 
   window.dataLayer = window.dataLayer || [];
@@ -16,5 +18,5 @@ export default function ga() {
   }
 
   gtag('js', new Date());
-  gtag('config', 'UA-149910123-1');
+  gtag('config', id);
 }
