4032a8913bd87b7e2f309ccb335f8f50d28a4fa2
Milson Júnior <junior.milson17@gmail.com>
2020-03-29 21:18:38 -0300

    chore: add exportTrailingSlash option

--- next.config.js
+++ next.config.js
@@ -13,76 +13,77 @@ const { PHASE_PRODUCTION_BUILD } = require('next-server/constants');
 
 const getBuildId = () => uuidv1();
 
-const getResolvedPath = dir => path.join(__dirname, dir);
+const getResolvedPath = (dir) => path.join(__dirname, dir);
 
 const setAbsolutePaths = (paths = [], currentConfig) =>
-  paths.reduce(
-    (acc, currentPath) => ({
-      ...acc,
-      [currentPath]: getResolvedPath(currentPath)
-    }),
-    currentConfig
-  );
+	paths.reduce(
+		(acc, currentPath) => ({
+			...acc,
+			[currentPath]: getResolvedPath(currentPath)
+		}),
+		currentConfig
+	);
 
 const currentBuildId = getBuildId();
 
 module.exports = withPlugins([
-  [withESLint],
-  [
-    withSass,
-    {
-      webpack: (config, _) => {
-        config.plugins = config.plugins || [];
+	[ withESLint ],
+	[
+		withSass,
+		{
+			webpack: (config, _) => {
+				config.plugins = config.plugins || [];
 
-        config.node = {
-          fs: 'empty',
-          net: 'empty',
-          tls: 'empty'
-        };
+				config.node = {
+					fs: 'empty',
+					net: 'empty',
+					tls: 'empty'
+				};
 
-        config.plugins = [
-          ...config.plugins,
+				config.plugins = [
+					...config.plugins,
 
-          new Dotenv({
-            path: path.join(__dirname, '.env'),
-            systemvars: true
-          }),
-          new webpack.DefinePlugin({
-            'process.env.BUILD_ID': JSON.stringify(currentBuildId)
-          })
-        ];
+					new Dotenv({
+						path: path.join(__dirname, '.env'),
+						systemvars: true
+					}),
+					new webpack.DefinePlugin({
+						'process.env.BUILD_ID': JSON.stringify(currentBuildId)
+					})
+				];
 
-        config.module.rules.push({
-          test: /\.(raw)(\?v=[/d]\.[/d]\.[/d])?$/,
-          use: 'raw-loader'
-        });
+				config.module.rules.push({
+					test: /\.(raw)(\?v=[/d]\.[/d]\.[/d])?$/,
+					use: 'raw-loader'
+				});
 
-        if (config.mode === 'production') {
-          if (Array.isArray(config.optimization.minimizer)) {
-            config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
-          }
-        }
+				if (config.mode === 'production') {
+					if (Array.isArray(config.optimization.minimizer)) {
+						config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+					}
+				}
 
-        const newAliasConfig = setAbsolutePaths(
-          ['components', 'pages', 'layouts', 'utils', 'styles'],
-          config.resolve.alias
-        );
+				const newAliasConfig = setAbsolutePaths(
+					[ 'components', 'pages', 'layouts', 'utils', 'styles' ],
+					config.resolve.alias
+				);
 
-        config.resolve.alias = newAliasConfig;
+				config.resolve.alias = newAliasConfig;
 
-        return config;
-      },
-      cssModules: true,
-      cssLoaderOptions: {
-        localIdentName: '[local]___[hash:base64:3]'
-      },
-      generateBuildId: () => currentBuildId,
-      [PHASE_PRODUCTION_BUILD]: {
-        cssLoaderOptions: {
-          localIdentName: '[hash:base64:8]'
-        }
-      },
-      exportPathMap: routes
-    }
-  ]
+				return config;
+			},
+			cssModules: true,
+			cssLoaderOptions: {
+				localIdentName: '[local]___[hash:base64:3]'
+			},
+			generateBuildId: () => currentBuildId,
+			[PHASE_PRODUCTION_BUILD]: {
+				cssLoaderOptions: {
+					localIdentName: '[hash:base64:8]'
+				}
+			},
+			exportTrailingSlash: true,
+			exportPathMap: routes
+		}
+	]
 ]);
