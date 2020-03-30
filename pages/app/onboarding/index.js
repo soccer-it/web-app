2dccc02c2386565487f8e3a93ed19261b541b103
Milson Júnior <junior.milson17@gmail.com>
2020-03-29 21:16:28 -0300

    fix: app pages structure

---
+++ pages/app/onboarding/index.js
@@ -0,0 +1,14 @@
+import BaseLayout from 'layouts/BaseLayout';
+import Head from 'next/head';
+import Onboarding from 'components/Onboarding';
+
+export default function OnboardingPage() {
+  return (
+    <BaseLayout pageName="onboarding">
+      <Head>
+        <title key="title">soccerit | Seja bem-vindo</title>
+      </Head>
+      <Onboarding />
+    </BaseLayout>
+  );
+}
