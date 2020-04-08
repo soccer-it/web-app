67982396bd67fb3d6c3831bc24f549d91cbb7be6
Cesar <cesar2012oliveira@gmail.com>
2020-04-08 00:38:02 -0300

    formControl support custom labels now

--- components/FormControl/index.js
+++ components/FormControl/index.js
@@ -4,14 +4,19 @@ import Icon from 'components/Icon';
 // Styles
 import { navigationWrapper, buttonNext, buttonPrev, buttonIcon } from './FormControl.scss';
 
-export default function Steps({ onPrev, onNext }) {
+export default function Steps({
+  onPrev,
+  onNext,
+  nextLabel = "continuar",
+  prevLabel = "voltar"
+}) {
   return (
     <div className={navigationWrapper}>
       <button data-prev className={buttonPrev} onClick={onPrev}>
-        Voltar
+        {prevLabel}
       </button>
       <button data-next onClick={onNext} className={buttonNext}>
-        Continuar
+        {nextLabel}
         <Icon id="arrow-right" className={buttonIcon} />
       </button>
     </div>
